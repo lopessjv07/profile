d6ac3403e2e592a51ed4f1738bfc6595283b87fe
João Vitor Lopes <joaovitorlps07@hotmail.com>
2025-12-23 00:10:06 -0300

    correcao bug logo

--- profile/src/components/Header/Header.tsx
+++ profile/src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
     <>
       <header>
         <div className="logo">
-          <img src="/public/logo.png" alt="Logo" width="150px" />
+          <img src="../../assets/logo.png" alt="Logo" width="150px" />
         </div>
         <div className="navLinks">
           <a href="https://www.instagram.com/lopessdev/" target="_blank">
